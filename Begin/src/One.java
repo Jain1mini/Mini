e69b1523a397b54b16a29a479797da8aacf7c8eb
mini <minijain8171@gmail.com>
2022-05-26 13:26:20 +0800

    Core Java

---
+++ Begin/src/One.java
@@ -0,0 +1,20 @@
+
+
+
+public class One {
+	
+	void run()
+	{
+		System.out.println("vehicle is running");
+	}
+}
+	class Bike extends One
+	{
+		
+		public static void main(String[] args) {
+	Bike obj = new Bike();
+	obj.run();
+		
+		 
+	}
+}
